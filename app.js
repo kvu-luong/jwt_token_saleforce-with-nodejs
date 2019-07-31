2cef94cac134d40f75a39cb7068edf764a88ba08
kvu-luong <luongkhanhvu1392@gmail.com>
2019-07-31 16:39:21 +0700

    get access token from salesforce with nodejs

---
+++ app.js
@@ -0,0 +1,31 @@
+var request = require('request');
+var jwt = require('jsonwebtoken');
+
+var key = require('fs').readFileSync('./crt/privateKey.key', 'utf8');
+
+var options = {
+    issuer: '3MVG9vtcvGoeH2bjL78f3tlzuhfdNM8oWYZGB7uLJirIeF5dZCRcJwBAkqekf.lVa6H6Gqi864Som7hjzXPLo',
+    audience: 'https://login.salesforce.com',
+    expiresIn: 3,
+    algorithm: 'RS256'
+}
+var token = jwt.sign({ prn: 'vuluongks@none.com'}, key, options);
+
+var post = {
+    uri: 'https://login.salesforce.com/services/oauth2/token',
+    form: {
+       'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+       'assertion':  token
+    },
+    method: 'post'
+}
+console.log('<<<<Start>>>>');
+console.log(post);
+
+console.log('<<<<Stop>>>>');
+
+request(post, function(err, res, body) {
+    console.log(err);
+    console.log(res.statusCode);
+    console.log(body);
+});
